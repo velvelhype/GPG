eeac9b5886f90b8996ff0f53b51cec6ba9124073
velvelhype <velvelhype@gmail.com>
2022-04-27 01:55:24 +0900

    add:gitignore mod

--- go-00/ex06/vendor/piscine/printcombn.go
+++ go-00/ex06/vendor/piscine/printcombn.go
@@ -50,34 +50,3 @@ func PrintCombN(n int){
 	count_up_and_print(digits, n, 0)
 	ft.PrintRune('\n');
 }
-
-// func PrintCombN(){
-// 	var first int;
-// 	var second int;
-// 	var third int;
-
-// 	first = 0;
-// 	second = 1;
-// 	third = 2;
-// 	for (first <= 7) {
-// 		for (second <= 8) {
-// 			for (third <= 9){
-// 				ft.PrintRune(rune('0' + first));
-// 				ft.PrintRune(rune('0' + second));
-// 				ft.PrintRune(rune('0' + third));
-// 				if first == 7 && second == 8 && third == 9 {
-// 					break;
-// 				}
-// 				ft.PrintRune(',')
-// 				ft.PrintRune(' ')
-// 				third++;
-// 			}
-// 			second++;
-// 			third = second + 1;
-// 		}
-// 		first++;
-// 		second = first + 1;
-// 		third = second + 1;
-// 	}
-// 	ft.PrintRune('\n');
-// }
