9bff29431be34a403894b10e7e4ed41309461ec2
velvelhype <velvelhype@gmail.com>
2022-05-07 22:58:09 +0900

    add:randomtest

--- GPGRUSH_02/main.go
+++ GPGRUSH_02/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math/rand"
 import "piscine"
 
 func output(str string, board [][]string) {
@@ -21,14 +22,46 @@ func output(str string, board [][]string) {
 	}
 }
 
+func randFit()string {
+	if rand.Intn(7) == 1 {
+		return (string)('0' + rand.Intn(8) + 1)
+	} else {
+		return (string)('.')
+	}
+}
+
 func main() {
+	fmt.Println("//////////////////////////////////////////");
+	fmt.Println("random maps");
+	fmt.Println("//////////////////////////////////////////\n");
+	board := [][]string{
+		{
+			".....",
+			".....",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	for i := 0; i < 100; i++{
+		for i := 0; i < 5; i++{
+			board[0][i] = randFit();
+			board[0][i] += randFit();
+			board[0][i] += randFit();
+			board[0][i] += randFit();
+			board[0][i] += randFit();
+		}
+	output("rand_test", board)
+	}
+	return
+
 	fmt.Println("//////////////////////////////////////////");
 	fmt.Println("valid maps");
 	fmt.Println("//////////////////////////////////////////\n");
 
 	fmt.Println("\n----success_cases-----");
 
-	board := [][]string{
+	board = [][]string{
 		{
 			"...2.",
 			"..6.4",
@@ -106,18 +139,6 @@ func main() {
 			".....",
 		},
 	}
-	output("empty", board)
-
-	board = [][]string{
-		{
-			"1....",
-			".....",
-			".....",
-			".....",
-			".....",
-		},
-	}
-	output("inv_number", board)
 
 	fmt.Println("//////////////////////////////////////////");
 	fmt.Println("invalid maps---------");
@@ -141,7 +162,304 @@ func main() {
 	}
 	output("pdf_inv2", board)
 
+	board = [][]string{
+		{
+			"BBBBB",
+			"BBBBB",
+			"BB9BB",
+			"BBBBB",
+			"BBBBB",
+		},
+	}
+	output("pdf_inv2", board)
+
+	board = [][]string{
+		{
+			".....",
+			".....",
+			"B....",
+			".....",
+			".....",
+		},
+	}
+	output("B_contaminated", board)
+	return;
+
 	fmt.Println("//////////////////////////////////////////");
 	fmt.Println("random ---------");
-	fmt.Println("//////////////////////////////////////////\n");	
+	fmt.Println("//////////////////////////////////////////\n");
+
+	board = [][]string{
+		{
+			"...1.",
+			"..6.4",
+			"5..3.",
+			"7.6..",
+			".3...",
+		},
+	}
+	output("random", board)
+
+	board = [][]string{
+		{
+			".....",
+			".....",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"...2.",
+			"..6.4",
+			"5....",
+			"7.6..",
+			".3...",
+		},
+	}
+	output("random", board)
+
+	board = [][]string{
+		{
+			"...1.",
+			"..6.4",
+			"5....6",
+			"7.6..",
+			".3...",
+		},
+	}
+	output("random", board)
+
+	board = [][]string{
+		{
+			"...3.",
+			"..6.4",
+			"5....6",
+			"7.6..",
+			".3...",
+		},
+	}
+	output("random", board)
+
+	board = [][]string{
+		{
+			"...6.",
+			"..6.4",
+			"5....6",
+			"7.6..",
+			".3...",
+		},
+	}
+	output("random", board)
+
+	board = [][]string{
+		{
+			"...1.",
+			"..6.4",
+			"5....",
+			"7.6..",
+			".3...",
+		},
+	}
+	output("random", board)
+
+	board = [][]string{
+		{
+			".....",
+			".....",
+			"..3..",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+	board = [][]string{
+		{
+			".....",
+			".....",
+			"..3..",
+			"...5.",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".....",
+			".22..",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".....",
+			".2...",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".....",
+			".3.3.",
+			".....",
+			".4.3.",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".....",
+			".33..",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".....",
+			".88..",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".....",
+			".4.4.",
+			".4.4.",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"5....",
+			".5...",
+			"..5..",
+			"...5.",
+			"....5",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"6....",
+			".....",
+			".6...",
+			"...6.",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"7....",
+			".7...",
+			"..7..",
+			"...7.",
+			"....7",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			".3...",
+			"..3..",
+			"..3..",
+			"..3..",
+			"..3..",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"..3..",
+			"..3..",
+			"..3..",
+			"..3..",
+			"..3..",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"..5..",
+			"..5..",
+			"..5..",
+			"..5..",
+			"..5..",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"...3.",
+			".5.3.",
+			".....",
+			".2...",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"...4.",
+			".5...",
+			".....",
+			".37..",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"3...5",
+			".....",
+			".....",
+			".....",
+			".....",
+		},
+	}
+	output("empty", board)
+
+	board = [][]string{
+		{
+			"99999",
+			"99999",
+			"99999",
+			"99999",
+			"99999",
+		},
+	}
+	output("empty", board)
 }
